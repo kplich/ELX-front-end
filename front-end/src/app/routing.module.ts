1cb01511d3b0ef73bf445aca04397ec77f0bf0db
Kamil Plich <kamilplich2@gmail.com>
2020-02-22 00:57:40 +0100

    same as before

--- front-end/src/app/routing.module.ts
+++ front-end/src/app/routing.module.ts
@@ -4,8 +4,10 @@ import {RouterModule, Routes} from '@angular/router';
 import {ItemBrowsingComponent} from './item-browsing/item-browsing.component';
 import {MyAccountComponent} from './my-account/my-account.component';
 import {OfferComponent} from './offer/offer.component';
+import {LoggingInComponent} from './identity-management/logging-in/logging-in.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoggingInComponent},
   {path: 'browse-items', component: ItemBrowsingComponent},
   {path: 'my-account', component: MyAccountComponent},
   {path: 'offer', component: OfferComponent}
