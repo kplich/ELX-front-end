7dceef45a78823fceccb7f67e73a8a313ff7ebe1
Kamil Plich <kamilplich2@gmail.com>
2020-07-06 22:15:11 +0200

    fix jasmine tests
    Signed-off-by: Kamil Plich <kamilplich2@gmail.com>

--- src/app/identity-management/identity-management.service.spec.ts
+++ src/app/identity-management/identity-management.service.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 
 import { IdentityManagementService } from './identity-management.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('IdentityManagementService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({imports: [HttpClientTestingModule]}));
 
   it('should be created', () => {
     const service: IdentityManagementService = TestBed.get(IdentityManagementService);
