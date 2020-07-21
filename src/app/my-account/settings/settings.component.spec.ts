eeb36e3bae8b2c309a6ac3c554d5781cc9796ed8
kplich <kamil.plich@iteratec.com>
2020-07-21 23:58:04 +0200

    refactor: extract messages to object; test: changing password component

--- src/app/my-account/settings/settings.component.spec.ts
+++ src/app/my-account/settings/settings.component.spec.ts
@@ -1,25 +1,122 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SettingsComponent } from './settings.component';
+import { SettingsComponent, OLD_PASSWORD_LABEL, NEW_PASSWORD_LABEL, NEW_PASSWORD_HINT, BUTTON_CHANGE_PASSWORD_TEXT, OLD_PASSWORD_REQUIRED_MESSAGE, NEW_PASSWORD_REQUIRED_MESSAGE, PASSWORDS_EQUAL_MESSAGE, PASSWORD_PATTERN_MESSAGE } from './settings.component';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { MaterialModule } from 'src/app/material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthenticationService } from 'src/app/identity-management/authentication-service/authentication.service';
+import { Router } from '@angular/router';
+import { SnackBarService } from 'src/app/shared/snack-bar-service/snack-bar.service';
+import { MatFormFieldHarness } from '@angular/material/form-field/testing';
+import { MatInputHarness } from '@angular/material/input/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { By } from '@angular/platform-browser';
 
 describe('SettingsComponent', () => {
-  let component: SettingsComponent;
+  const authenticationServiceSpy = jasmine.createSpyObj('AuthenticationService', ['signUp']);
+  const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+  const snackBarServiceSpy = jasmine.createSpyObj('SnackBarService', ['openSnackBar']);
+
   let fixture: ComponentFixture<SettingsComponent>;
+  let loader: HarnessLoader;
+
+  let oldPasswordFormField: MatFormFieldHarness;
+  let oldPasswordInput: MatInputHarness;
+  let newPasswordFormField: MatFormFieldHarness;
+  let newPasswordInput: MatInputHarness;
+  let changePasswordButton: MatButtonHarness;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SettingsComponent ]
-    })
-    .compileComponents();
+      imports: [MaterialModule, BrowserAnimationsModule, ReactiveFormsModule],
+      declarations: [ SettingsComponent ],
+      providers: [
+        {provide: AuthenticationService, useValue: authenticationServiceSpy},
+        {provide: Router, useValue: routerSpy},
+        {provide: SnackBarService, useValue: snackBarServiceSpy}
+      ]
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(SettingsComponent);
+      loader = TestbedHarnessEnvironment.loader(fixture);
+
+      loader.getHarness(MatFormFieldHarness.with({selector: '#old-password-form-field'})).then(harness => {
+        oldPasswordFormField = harness;
+      });
+      loader.getHarness(MatInputHarness.with({selector: '#old-password-form-field input'})).then(harness => {
+        oldPasswordInput = harness;
+      });
+      loader.getHarness(MatFormFieldHarness.with({selector: '#new-password-form-field'})).then(harness => {
+        newPasswordFormField = harness;
+      });
+      loader.getHarness(MatInputHarness.with({selector: '#new-password-form-field input'})).then(harness => {
+        newPasswordInput = harness;
+      });
+      loader.getHarness(MatButtonHarness.with({selector: '#change-password-button'})).then(harness => {
+        changePasswordButton = harness;
+
+
+      });
+    });
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SettingsComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be created and displayed correctly', async(async () => {
+    expect(fixture.componentInstance).toBeTruthy();
+
+    expect(await oldPasswordFormField.getLabel()).toEqual(OLD_PASSWORD_LABEL);
+
+    expect(await newPasswordFormField.getLabel()).toEqual(NEW_PASSWORD_LABEL);
+    expect((await newPasswordFormField.getTextHints())[0]).toEqual(NEW_PASSWORD_HINT);
+
+    expect(fixture.componentInstance.passwordChangeForm.valid).toBeFalsy();
+
+    expect(await changePasswordButton.getText()).toEqual(BUTTON_CHANGE_PASSWORD_TEXT);
+    expect(await changePasswordButton.isDisabled()).toBeTruthy();
+  }));
+
+  it('should display errors after touching the fields', async () => {
+    await oldPasswordInput.focus();
+    await oldPasswordInput.blur();
+
+    expect(await oldPasswordFormField.hasErrors()).toBeTruthy();
+    expect((await oldPasswordFormField.getTextErrors())[0]).toEqual(OLD_PASSWORD_REQUIRED_MESSAGE);
+
+    await newPasswordInput.focus();
+    await newPasswordInput.blur();
+
+    expect(await newPasswordFormField.hasErrors()).toBeTruthy();
+    expect((await newPasswordFormField.getTextErrors())[0]).toEqual(NEW_PASSWORD_REQUIRED_MESSAGE);
   });
+
+  it('should show an error when new password doesn\'t match the pattern', async () => {
+    await newPasswordInput.setValue('password');
+    await newPasswordInput.blur();
+
+    expect(await newPasswordFormField.isControlValid()).toBeFalsy();
+    expect(await newPasswordFormField.hasErrors()).toBeTruthy();
+    expect((await newPasswordFormField.getTextErrors())[0]).toEqual(PASSWORD_PATTERN_MESSAGE);
+  });
+
+
+  it('should show an error when new passwords match', async () => {
+    const matchingPassword = 'P@ssw0rd';
+    await oldPasswordInput.setValue(matchingPassword);
+    await oldPasswordInput.blur();
+
+    await newPasswordInput.setValue(matchingPassword);
+    await newPasswordInput.blur();
+
+    expect(await oldPasswordFormField.isControlValid()).toBeTruthy('Old password input should be valid!');
+    expect(await newPasswordFormField.isControlValid()).toBeTruthy('New password input should be valid!');
+    expect(await newPasswordFormField.hasErrors()).toBeFalsy('New password input shouldn\'t display errors!');
+    expect(await changePasswordButton.isDisabled()).toBeTruthy('Password change button should be disabled!');
+  });
+
+
 });
--- src/app/my-account/settings/settings.component.ts
+++ src/app/my-account/settings/settings.component.ts
@@ -1,97 +1,113 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators, Form} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
-import {AuthenticationService} from '../../identity-management/authentication-service/authentication.service';
-import {PasswordChangeRequest} from '../../identity-management/authentication-service/PasswordChangeRequest';
-import {SnackBarService} from '../../shared/snack-bar-service/snack-bar.service';
-import { HttpErrorResponse } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from "@angular/forms";
+import { ErrorStateMatcher } from "@angular/material/core";
+import { AuthenticationService } from "../../identity-management/authentication-service/authentication.service";
+import { PasswordChangeRequest } from "../../identity-management/authentication-service/PasswordChangeRequest";
+import { SnackBarService } from "../../shared/snack-bar-service/snack-bar.service";
+import { HttpErrorResponse } from "@angular/common/http";
+import { Router } from "@angular/router";
 
-export const ACCOUNT_SETTINGS_TITLE = 'Account settings';
-export const ACCOUNT_SETTINGS_DESCRIPTION = 'Change password, link your Ethereum wallet, etc.';
+export const ACCOUNT_SETTINGS_TITLE = "Account settings";
+export const ACCOUNT_SETTINGS_DESCRIPTION =
+  "Change password, link your Ethereum wallet, etc.";
 
-export const CHANGE_PASSWORD_TITLE = 'Change password';
+export const CHANGE_PASSWORD_TITLE = "Change password";
 
-export const OLD_PASSWORD_LABEL = 'Old password';
-export const NEW_PASSWORD_LABEL = 'New password';
+export const OLD_PASSWORD_LABEL = "Old password";
+export const NEW_PASSWORD_LABEL = "New password";
 export const MINIMUM_PASSWORD_LENGTH = 8;
 export const MAXIMUM_PASSWORD_LENGTH = 40;
-export const PASSWORD_HINT = `Password has to be ${MINIMUM_PASSWORD_LENGTH}-${MAXIMUM_PASSWORD_LENGTH} characters long (including a lowercase letter, an uppercase letter, a digit and a special character) and different from the old password.`;
-export const OLD_PASSWORD_REQUIRED_MESSAGE = 'Old password is required!';
-export const NEW_PASSWORD_REQUIRED_MESSAGE = 'New password is required!';
+export const NEW_PASSWORD_HINT = `Password has to be ${MINIMUM_PASSWORD_LENGTH}-${MAXIMUM_PASSWORD_LENGTH} characters long (including a lowercase letter, an uppercase letter, a digit and a special character) and different from the old password.`;
+export const OLD_PASSWORD_REQUIRED_MESSAGE = "Old password is required!";
+export const NEW_PASSWORD_REQUIRED_MESSAGE = "New password is required!";
 export const MINIMUM_PASSWORD_LENGTH_MESSAGE = `The password needs to be at least ${MINIMUM_PASSWORD_LENGTH} characters long.`;
 export const MAXIMUM_PASSWORD_LENGTH_MESSAGE = `The password needs to be at most ${MAXIMUM_PASSWORD_LENGTH} characters long.`;
-export const PASSWORD_PATTERN_MESSAGE = 'The password needs to have a lowercase and an uppercase letter, a digit and a special character.';
-export const PASSWORDS_EQUAL_MESSAGE = 'Passwords can\'t be equal.';
-export const CHANGE_PASSWORD_BUTTON_TEXT = 'Change password';
+export const PASSWORD_PATTERN_MESSAGE =
+  "The password needs to have a lowercase and an uppercase letter, a digit and a special character.";
+export const PASSWORDS_EQUAL_MESSAGE = "Passwords can't be equal.";
+export const BUTTON_CHANGE_PASSWORD_TEXT = "Change password";
 
-export const PASSWORDS_MATCH_TAG = 'passwordsmatch';
+export const PASSWORDS_MATCH_TAG = "passwordsmatch";
 
-export const INVALID_DATA_MESSAGE = 'Invalid request data!';
-export const SERVER_ERROR_MESSAGE = 'Server error, try again!';
-export const PASSWORD_CHANGED_SUCCESSFULLY_MESSAGE = 'Changed password successfully!';
+export const INVALID_DATA_MESSAGE = "Invalid request data!";
+export const SERVER_ERROR_MESSAGE = "Server error, try again!";
+export const PASSWORD_CHANGED_SUCCESSFULLY_MESSAGE =
+  "Changed password successfully!";
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
     const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
   }
 }
 
 export function passwordsNotEqualValidator(formGroup: FormGroup) {
-  const oldPasswordInput = formGroup.get('oldPasswordInput');
-  const newPasswordInput = formGroup.get('newPasswordInput');
+  const oldPasswordInput = formGroup.get("oldPasswordInput");
+  const newPasswordInput = formGroup.get("newPasswordInput");
 
-  return oldPasswordInput.value !== newPasswordInput.value
-    ? null : {'passwordsmatch': true};
+  return oldPasswordInput.value != newPasswordInput.value
+    ? null
+    : { passwordsmatch: true };
 }
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  selector: "app-settings",
+  templateUrl: "./settings.component.html",
+  styleUrls: ["./settings.component.scss"],
 })
 export class SettingsComponent implements OnInit {
-
-  accountSettingsTitle = ACCOUNT_SETTINGS_TITLE;
-  accountSettingsDescription = ACCOUNT_SETTINGS_DESCRIPTION;
-
-  changePasswordTitle = CHANGE_PASSWORD_TITLE;
-
-  oldPasswordLabel = OLD_PASSWORD_LABEL;
-  oldPasswordRequiredMessage = OLD_PASSWORD_REQUIRED_MESSAGE;
-
-  newPasswordLabel = NEW_PASSWORD_LABEL;
-  newPasswordHint = PASSWORD_HINT;
-  newPasswordRequiredMessage = NEW_PASSWORD_REQUIRED_MESSAGE;
-  minimumPasswordLengthMessage = MINIMUM_PASSWORD_LENGTH_MESSAGE;
-  maximumPasswordLengthMessage = MAXIMUM_PASSWORD_LENGTH_MESSAGE;
-  passwordPatternMessage = PASSWORD_PATTERN_MESSAGE;
-  passwordsEqualMessage = PASSWORDS_EQUAL_MESSAGE;
-
-  changePasswordButtonText = CHANGE_PASSWORD_BUTTON_TEXT;
+  passwordChangeMessages = {
+    accountSettingsTitle: ACCOUNT_SETTINGS_TITLE,
+    accountSettingsDescription: ACCOUNT_SETTINGS_DESCRIPTION,
+    changePasswordTitle: CHANGE_PASSWORD_TITLE,
+    oldPasswordLabel: OLD_PASSWORD_LABEL,
+    oldPasswordRequiredMessage: OLD_PASSWORD_REQUIRED_MESSAGE,
+    newPasswordLabel: NEW_PASSWORD_LABEL,
+    newPasswordHint: NEW_PASSWORD_HINT,
+    newPasswordRequiredMessage: NEW_PASSWORD_REQUIRED_MESSAGE,
+    minimumPasswordLengthMessage: MINIMUM_PASSWORD_LENGTH_MESSAGE,
+    maximumPasswordLengthMessage: MAXIMUM_PASSWORD_LENGTH_MESSAGE,
+    passwordPatternMessage: PASSWORD_PATTERN_MESSAGE,
+    passwordsEqualMessage: PASSWORDS_EQUAL_MESSAGE,
+    changePasswordButtonText: BUTTON_CHANGE_PASSWORD_TEXT};
 
   // language=JSRegexp
-  containsUppercaseLetterPattern = '.*[A-Z]+.*';
+  containsUppercaseLetterPattern = ".*[A-Z]+.*";
   // language=JSRegexp
-  containsLowercaseLetterPattern = '.*[a-z]+.*';
+  containsLowercaseLetterPattern = ".*[a-z]+.*";
   // language=JSRegexp
-  containsDigitPattern = '.*\\d+.*';
+  containsDigitPattern = ".*\\d+.*";
   // language=JSRegexp
-  containsSpecialCharacterPattern = '.*[\\W_]+.*';
+  containsSpecialCharacterPattern = ".*[\\W_]+.*";
 
-  passwordChangeForm: FormGroup = new FormGroup({
-    oldPasswordInput: new FormControl('', [Validators.required]),
-    newPasswordInput: new FormControl('', [
-      Validators.required,
-      Validators.pattern(this.containsLowercaseLetterPattern),
-      Validators.pattern(this.containsUppercaseLetterPattern),
-      Validators.pattern(this.containsDigitPattern),
-      Validators.pattern(this.containsSpecialCharacterPattern),
-      Validators.minLength(MINIMUM_PASSWORD_LENGTH),
-      Validators.maxLength(MAXIMUM_PASSWORD_LENGTH)
-    ])
-  }, passwordsNotEqualValidator);
+  passwordChangeForm: FormGroup = new FormGroup(
+    {
+      oldPasswordInput: new FormControl("", [Validators.required]),
+      newPasswordInput: new FormControl("", [
+        Validators.required,
+        Validators.pattern(this.containsLowercaseLetterPattern),
+        Validators.pattern(this.containsUppercaseLetterPattern),
+        Validators.pattern(this.containsDigitPattern),
+        Validators.pattern(this.containsSpecialCharacterPattern),
+        Validators.minLength(MINIMUM_PASSWORD_LENGTH),
+        Validators.maxLength(MAXIMUM_PASSWORD_LENGTH),
+      ]),
+    },
+    passwordsNotEqualValidator
+  );
 
   errorStateMatcher: ErrorStateMatcher = new MyErrorStateMatcher();
 
@@ -99,67 +115,78 @@ export class SettingsComponent implements OnInit {
     private authenticationService: AuthenticationService,
     private router: Router,
     private snackBarService: SnackBarService
-  ) {
+  ) {}
 
-  }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   sendPasswordChangeRequest() {
     console.log(this.passwordChangeRequest);
-    this.authenticationService.changePassword(this.passwordChangeRequest).subscribe({
-      next: _ => {
-        this.authenticationService.logOut().then(_ => {
-          this.router.navigateByUrl('/browse-items').then(_ => {
-            this.snackBarService.openSnackBar(PASSWORD_CHANGED_SUCCESSFULLY_MESSAGE);
-          })
-        });
-      },
-      error: error => this.openSnackBarOnError(error)
-    });
+    this.authenticationService
+      .changePassword(this.passwordChangeRequest)
+      .subscribe({
+        next: (_) => {
+          this.authenticationService.logOut().then((_) => {
+            this.router.navigateByUrl("/browse-items").then((_) => {
+              this.snackBarService.openSnackBar(
+                PASSWORD_CHANGED_SUCCESSFULLY_MESSAGE
+              );
+            });
+          });
+        },
+        error: (error) => this.openSnackBarOnError(error),
+      });
   }
 
   get oldPasswordInput(): FormControl {
-    return this.passwordChangeForm.get('oldPasswordInput') as FormControl;
+    return this.passwordChangeForm.get("oldPasswordInput") as FormControl;
   }
 
   get newPasswordInput(): FormControl {
-    return this.passwordChangeForm.get('newPasswordInput') as FormControl;
+    return this.passwordChangeForm.get("newPasswordInput") as FormControl;
   }
 
   get passwordChangeRequest(): PasswordChangeRequest {
-    return {oldPassword: this.oldPasswordInput.value, newPassword: this.newPasswordInput.value}
+    return {
+      oldPassword: this.oldPasswordInput.value,
+      newPassword: this.newPasswordInput.value,
+    };
   }
 
   get oldPasswordNotProvided(): boolean {
-    return this.oldPasswordInput.hasError('required');
+    return this.oldPasswordInput.hasError("required");
   }
 
   get newPasswordNotProvided(): boolean {
-    return this.newPasswordInput.hasError('required');
+    return this.newPasswordInput.hasError("required");
   }
 
   get newPasswordTooShort(): boolean {
-    return this.newPasswordInput.hasError('minlength');
+    return this.newPasswordInput.hasError("minlength");
   }
 
   get newPasswordTooLong(): boolean {
-    return this.newPasswordInput.hasError('maxlength');
+    return this.newPasswordInput.hasError("maxlength");
   }
 
   get newPasswordDoesntMatchPattern(): boolean {
-    return !this.newPasswordTooShort && !this.newPasswordTooLong
-        && this.newPasswordInput.hasError('pattern');
+    return (
+      !this.newPasswordTooShort &&
+      !this.newPasswordTooLong &&
+      this.newPasswordInput.hasError("pattern")
+    );
   }
 
   get passwordsAreEqual(): boolean {
-    return this.passwordChangeForm.touched
-      && this.passwordChangeForm.dirty
-      && !this.newPasswordTooShort
-      && !this.newPasswordTooLong
-      && !this.newPasswordInput.hasError('patter')
-      && this.passwordChangeForm.hasError('passwordsmatch');
+
+
+    return (
+      this.newPasswordInput.dirty &&
+      this.oldPasswordInput.dirty &&
+      !this.newPasswordTooShort &&
+      !this.newPasswordTooLong &&
+      !this.newPasswordInput.hasError("pattern") &&
+      this.passwordChangeForm.hasError("passwordsmatch")
+    );
   }
 
   private openSnackBarOnError(errorResponse: HttpErrorResponse) {
@@ -174,5 +201,4 @@ export class SettingsComponent implements OnInit {
       }
     }
   }
-
 }
