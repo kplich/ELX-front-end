ebfacb3f0d44e4447940e960dc7d440be67ee33f
Kamil Plich <kamilplich2@gmail.com>
2020-09-22 21:00:02 +0200

    feat: add ethereum address to registration form
    Signed-off-by: Kamil Plich <kamilplich2@gmail.com>

--- src/app/authentication/registration/registration.component.ts
+++ src/app/authentication/registration/registration.component.ts
@@ -10,37 +10,45 @@ import {MyErrorStateMatcher} from "@shared/MyErrorStateMatcher";
 import {LOGGED_IN_SUCCESSFULLY_MESSAGE} from "../logging-in/logging-in.component";
 import {LoggedInUserService} from "@shared/logged-in-user/logged-in-user.service";
 
-export const USERNAME_LABEL = "Username";
-export const USERNAME_REQUIRED_MESSAGE = "A username is required!";
 export const MINIMUM_USERNAME_LENGTH = 3;
-export const MINIMUM_USERNAME_LENGTH_MESSAGE
-    = `The username needs to be at least ${MINIMUM_USERNAME_LENGTH} characters long.`;
 export const MAXIMUM_USERNAME_LENGTH = 20;
-export const MAXIMUM_USERNAME_LENGTH_MESSAGE
-    = `The username needs to be at most ${MAXIMUM_USERNAME_LENGTH} characters long.`;
-export const USERNAME_PATTERN_MESSAGE = "Username can only contain letters, digits and underscores (_).";
-export const USERNAME_HINT
-    = `Username has to be ${MINIMUM_USERNAME_LENGTH}-${MAXIMUM_USERNAME_LENGTH} characters long and can only contain letters, digits and underscores (_).`;
 
-export const PASSWORD_LABEL = "Password";
-export const PASSWORD_REQUIRED_MESSAGE = "A password is required!";
 export const MINIMUM_PASSWORD_LENGTH = 8;
-export const MINIMUM_PASSWORD_LENGTH_MESSAGE
-    = `The password needs to be at least ${MINIMUM_PASSWORD_LENGTH} characters long.`;
 export const MAXIMUM_PASSWORD_LENGTH = 40;
-export const MAXIMUM_PASSWORD_LENGTH_MESSAGE
-    = `The password needs to be at most ${MAXIMUM_PASSWORD_LENGTH} characters long.`;
-export const PASSWORD_PATTERN_MESSAGE
-    = "The password needs to have a lowercase and an uppercase letter, a digit and a special character.";
-export const PASSWORD_HINT
-    = `Password has to be ${MINIMUM_PASSWORD_LENGTH}-${MAXIMUM_PASSWORD_LENGTH} characters long (including a lowercase letter, an uppercase letter, a digit and a special character.)`;
 
-export const BUTTON_REGISTER_TEXT = "Register";
+export const ETHEREUM_ADDRESS_LENGTH = 42;
 
 export const INVALID_DATA_MESSAGE = "Invalid request data!";
 export const USER_ALREADY_EXISTS_MESSAGE = "User with such username already exists!";
 export const SERVER_ERROR_MESSAGE = "Server error, try again!";
 export const SIGNED_UP_SUCCESSFULLY_MESSAGE = "Signed up successfully!";
+
+export const STRINGS = {
+    username: {
+        label: "Username",
+        hint: `Username has to be ${MINIMUM_USERNAME_LENGTH}-${MAXIMUM_USERNAME_LENGTH} characters long and can only contain letters, digits and underscores (_).`,
+        required: "A username is required!",
+        tooShort: `The username needs to be at least ${MINIMUM_USERNAME_LENGTH} characters long.`,
+        tooLong: `The username needs to be at most ${MAXIMUM_USERNAME_LENGTH} characters long.`,
+        doesntMatchPattern: "Username can only contain letters, digits and underscores (_)."
+    },
+    password: {
+        label: "Password",
+        hint: `Password has to be ${MINIMUM_PASSWORD_LENGTH}-${MAXIMUM_PASSWORD_LENGTH} characters long (including a lowercase letter, an uppercase letter, a digit and a special character.)`,
+        required: "A password is required!",
+        tooShort: `The password needs to be at least ${MINIMUM_PASSWORD_LENGTH} characters long.`,
+        tooLong: `The password needs to be at most ${MAXIMUM_PASSWORD_LENGTH} characters long.`,
+        doesntMatchPattern: "The password needs to have a lowercase and an uppercase letter, a digit and a special character."
+    },
+    ethereumAddress: {
+        label: "Ethereum address",
+        hint: "Address of your Ethereum account - 42 characters, beginning with 0x. You don't need to provide it now, but you won't be able to send offers without it.",
+        tooShortOrTooLong: "The Ethereum address needs to be exactly 42 characters long."
+    },
+    button: {
+        register: "Register"
+    }
+};
 
 @Component({
     selector: "identity-registration",
@@ -49,27 +57,7 @@ export const SIGNED_UP_SUCCESSFULLY_MESSAGE = "Signed up successfully!";
 })
 export class RegistrationComponent implements OnInit {
 
-    readonly strings = {
-        username: {
-            label: USERNAME_LABEL,
-            hint: USERNAME_HINT,
-            required: USERNAME_REQUIRED_MESSAGE,
-            tooShort: MINIMUM_USERNAME_LENGTH_MESSAGE,
-            tooLong: MAXIMUM_USERNAME_LENGTH_MESSAGE,
-            doesntMatchPattern: USERNAME_PATTERN_MESSAGE
-        },
-        password: {
-            label: PASSWORD_LABEL,
-            hint: PASSWORD_HINT,
-            required: PASSWORD_REQUIRED_MESSAGE,
-            tooShort: MINIMUM_PASSWORD_LENGTH_MESSAGE,
-            tooLong: MAXIMUM_PASSWORD_LENGTH_MESSAGE,
-            doesntMatchPattern: PASSWORD_PATTERN_MESSAGE
-        },
-        button: {
-            register: BUTTON_REGISTER_TEXT
-        }
-    };
+    readonly strings = STRINGS;
 
     // language=JSRegexp
     readonly lettersDigitsUnderscoresPattern = "\\w*";
@@ -97,6 +85,10 @@ export class RegistrationComponent implements OnInit {
             Validators.pattern(this.containsSpecialCharacterPattern),
             Validators.minLength(MINIMUM_PASSWORD_LENGTH),
             Validators.maxLength(MAXIMUM_PASSWORD_LENGTH)
+        ]),
+        ethereumAddress: new FormControl("", [
+            Validators.minLength(ETHEREUM_ADDRESS_LENGTH),
+            Validators.maxLength(ETHEREUM_ADDRESS_LENGTH)
         ])
     });
 
@@ -128,7 +120,19 @@ export class RegistrationComponent implements OnInit {
                 doesntMatchPattern: () => !this.errors.password.tooShort()
                     && !this.errors.password.tooLong()
                     && this.controls.password.hasError("pattern")
+            },
+            ethereumAddress: {
+                tooShortOrTooLong: this.controls.ethereumAddress.hasError("minlength")
+                    || this.controls.ethereumAddress.hasError("maxlength")
             }
+        };
+    }
+
+    get neitherTouchedNorDirty() {
+        return {
+            username: !this.controls.username.dirty && !this.controls.username.touched,
+            password: !this.controls.password.dirty && !this.controls.password.touched,
+            ethereumAddress: !this.controls.ethereumAddress.dirty && !this.controls.ethereumAddress.touched
         };
     }
 
--- src/app/my-account/data/SimpleUser.ts
+++ src/app/my-account/data/SimpleUser.ts
@@ -1,14 +1,17 @@
 export class SimpleUser {
     id: number;
+    ethereumAddress: string | null;
     username: string;
 
     constructor(response: SimpleUserResponse) {
         this.id = response.id;
+        this.ethereumAddress = response.ethereumAddress;
         this.username = response.username;
     }
 }
 
 export interface SimpleUserResponse {
     id: number;
+    ethereumAddress: string | null;
     username: string;
 }
